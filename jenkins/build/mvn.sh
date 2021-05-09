c022afafb639361932766cb55f9949441170a787
EC2 Default User <ec2-user@ip-172-31-26-236.us-east-2.compute.internal>
2021-05-09 14:27:23 +0000

    modifies mavn app

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,4 +6,4 @@ echo "***************************"
 
 WORKSPACE=/home/ec2-user/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
 
-docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v  $WORKSPACE/simple-java-maven-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
